85ae57d8b7bf924032cb0f777bd76a1c111b66d9
kaguilar <keynner2908@gmail.com>
2025-12-23 15:16:35 -0400

    Acomoda la vista

---
+++ MauiApp1/Models/NotasViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MauiApp1.Models;
+
+public class NotasViewModel
+{
+    // Colección observable que se enlaza a la UI
+    public ObservableCollection<Nota> Notas { get; set; } = new ObservableCollection<Nota>();
+
+    // Comando para guardar una nueva nota
+    public ICommand GuardarNotaCommand { get; }
+
+    public NotasViewModel()
+    {
+        GuardarNotaCommand = new Command<string>(GuardarNota);
+    }
+
+    private void GuardarNota(string contenido)
+    {
+        var nuevaNota = new Nota
+        {
+            Titulo = "Nueva Nota",
+            Contenido = contenido
+        };
+
+        Notas.Add(nuevaNota);
+    }
+}
+    public class Nota
+    {
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+    }
+
--- MauiApp1/Views/NotePage.xaml.cs
+++ MauiApp1/Views/NotePage.xaml.cs
@@ -12,6 +12,14 @@ public partial class NotePage : ContentPage
 		
 	}
 
+    public class Nota
+    {
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+    }
+
+
     private async void SaveButton_Clicked(object sender, EventArgs e)
     {
 		File.WriteAllText(_fileName, TextEditor.Text);
