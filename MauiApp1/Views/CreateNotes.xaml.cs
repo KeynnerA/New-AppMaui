873954a931070a5d6471fac36e37b68b61ee4977
kaguilar <keynner2908@gmail.com>
2026-04-23 11:26:57 -0400

    Creacion de una nueva vista donde se crean las notas

--- MauiApp1/Views/CreateNotes.xaml.cs
+++ MauiApp1/Views/CreateNotes.xaml.cs
@@ -2,13 +2,125 @@ namespace MauiApp1.Views;
 
 public partial class CreateNotes : ContentPage
 {
-	public CreateNotes()
-	{
-		InitializeComponent();
-	}
-
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    public CreateNotes()
     {
-		await Launcher.Default.OpenAsync("https://thecodercave.com");
+        InitializeComponent();
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm)
+        {
+            if (string.IsNullOrWhiteSpace(vm.TituloEntryText))
+            {
+                await DisplayAlert("Advertencia",
+                                   "No hay título, coloque un título y podrá guardar la nota",
+                                   "OK");
+                return;
+            }
+
+            if (vm.GuardarNotaCommand.CanExecute(null))
+            {
+                vm.GuardarNotaCommand.Execute(null);
+                await DisplayAlert("Aviso", "Nota guardada", "OK");
+                AlarmPanel.IsVisible = false;
+            }
+        }
+    }
+
+
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm)
+        {
+            vm.LimpiarBorradorContenido();
+        }
+    }
+
+    private async void EditButton_Clicked(object sender, EventArgs e)
+    {
+        var action = await DisplayActionSheet("Opcion", "Cancelar", null, "Editar nota", "Eliminar nota");
+
+        if (action == "Editar nota")
+        {
+            if (BindingContext is MauiApp1.Models.NotasViewModel vm &&
+                sender is Button button &&
+                button.BindingContext is MauiApp1.Models.Nota nota)
+            {
+                vm.CargarNotaParaEdicion(nota);
+            }
+
+            if (BindingContext is MauiApp1.Models.NotasViewModel vmEdicion && vmEdicion.UsaEditorTextoPlano)
+            {
+                TextEditor.Focus();
+            }
+        }
+        else if (action == "Cancelar")
+        {
+
+        }
+        else if (action == "Eliminar nota") 
+        {
+            if (BindingContext is MauiApp1.Models.NotasViewModel vm &&
+                sender is Button button &&
+                button.BindingContext is MauiApp1.Models.Nota nota)
+            {
+                vm.EliminarNota(nota);
+            }
+        }
+
+
+    }
+
+    private void ToggleEditorCheckboxToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm)
+        {
+            vm.AlternarModoListaCheck();
+        }
+    }
+
+    private void AnadirLineaLista_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm)
+        {
+            vm.ItemsListaCheck.Add(new MauiApp1.Models.NotaItemLista());
+        }
+    }
+
+    private void EliminarLineaLista_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm &&
+            vm.ItemsListaCheck.Count > 0)
+        {
+            vm.ItemsListaCheck.RemoveAt(vm.ItemsListaCheck.Count - 1);
+        }
+    }
+
+    private void MostrarAlarmaToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm)
+        {
+            AlarmTimePicker.Time = vm.HoraAlarmaBorrador;
+            AlarmDatePicker.Date = vm.FechaAlarmaBorrador;
+        }
+
+        AlarmPanel.IsVisible = !AlarmPanel.IsVisible;
+    }
+
+    private async void GuardarAlarma_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is MauiApp1.Models.NotasViewModel vm)
+        {
+            vm.HoraAlarmaBorrador = AlarmTimePicker.Time;
+            vm.FechaAlarmaBorrador = AlarmDatePicker.Date;
+        }
+
+        var estado = (BindingContext as MauiApp1.Models.NotasViewModel)?.AlarmaActivaBorrador == true
+            ? $"Alarma colocada para {AlarmDatePicker.Date:dd/MM/yyyy} {AlarmDatePicker.Date.Add(AlarmTimePicker.Time):hh:mm tt}"
+            : "Alarma desactivada para esta nota";
+
+        await DisplayAlert("Alarma", estado, "OK");
     }
 }
